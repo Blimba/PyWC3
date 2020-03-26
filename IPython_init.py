b24934847d6c7fae949164bc24f6bfd46c832ad8
Bart Limburg <bartlimburg@gmail.com>
2020-03-26 17:32:24 +0100

    IPython version fix

--- IPython_init.py
+++ IPython_init.py
@@ -1,4 +1,4 @@
 from PyWC3 import Map
 from PyWC3 import Jass
-
-print("Welcome to PyWC3 version 1.0.0 by Bart Limburg\n\n")
+from PyWC3.__version__ import __version__
+print("Welcome to PyWC3 version {} by Bart Limburg\n\n".format(__version__))
