e885adaa1202700ae3474d9bfebe295e13a4f8ec
Blimba <bartlimburg@gmail.com>
2020-10-12 11:12:20 +0200

    Added suspend / resume for MouseEvent

--- pysrc/lib/mouseevent.py
+++ pysrc/lib/mouseevent.py
@@ -6,7 +6,7 @@ class MouseEvent:
     timer = None
     _lst = []
     _events = 0
-
+    _active = False
     def __init__(self, callback, *args):
         self.callback = callback
         self.args = args
@@ -23,7 +23,7 @@ class MouseEvent:
     @staticmethod
     def _click():
         # clicking anywhere apart from on the map gives 0.0 0.0 as coordinates
-        if BlzGetTriggerPlayerMouseX() != 0.0 and BlzGetTriggerPlayerMouseY() != 0.0:
+        if MouseEvent._active and (BlzGetTriggerPlayerMouseX() != 0.0 or BlzGetTriggerPlayerMouseY() != 0.0):
             MouseEvent._events += 1
             if MouseEvent._events > 1 and GetTriggerPlayer() != MouseEvent.player:
                 print('Two player mouse event ({} events), Player {} ignored!'.format(MouseEvent._events,GetPlayerId(GetTriggerPlayer())))
@@ -42,7 +42,7 @@ class MouseEvent:
 
     @staticmethod
     def _ordered():
-        if MouseEvent._events == 1:
+        if MouseEvent._active and MouseEvent._events == 1:
             MouseEvent.unit_order = GetIssuedOrderId()
             MouseEvent.ordered_unit = GetOrderedUnit()
 
@@ -57,8 +57,15 @@ class MouseEvent:
         for i in range(bj_MAX_PLAYERS):
             TriggerRegisterPlayerUnitEvent(t,Player(i),EVENT_PLAYER_UNIT_ISSUED_POINT_ORDER, None)
         TriggerAddAction(t,MouseEvent._ordered)
-
+        MouseEvent._active = True
         MouseEvent.timer = Timer()
 
+    @staticmethod
+    def suspend_events():
+        MouseEvent._active = False
+
+    @staticmethod
+    def resume_events():
+        MouseEvent._active = True
 
 AddScriptHook(MouseEvent._make_triggers, MAIN_BEFORE)
